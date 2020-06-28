af2e5941dac234eeb798d240220af801c37dd793
Fernando dos Santos <nando4803@gmail.com>
2020-06-28 19:02:41 +0100

    This commit add services to main-page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,13 @@ import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
-import Jobs from "../components/Jobs";
-import Projects from "../components/Projects";
 import Blogs from "../components/Blogs";
 
 export default () => {
   return (
     <Layout>
       <Hero />
+      <Services />
     </Layout>
   )
 }
