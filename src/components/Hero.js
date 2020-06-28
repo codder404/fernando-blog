d63090618c0b6fcd1f43fb14592080e0a5cae910
Fernando dos Santos <nando4803@gmail.com>
2020-06-28 18:01:39 +0100

    This commit add hero-content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -18,7 +18,28 @@ const query = graphql `
   `
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const { file:
+    { childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query);
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+              <h1>Ei, Fernando</h1>
+              <h4>Desenvolvedor Front-end</h4>
+              <Link to="/contact" className="btn">
+                contato
+              </Link>
+              <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+   </header>
+  )
 }
 
 export default Hero
