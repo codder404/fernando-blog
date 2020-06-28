8e5a37664c3e777a756576c3b5803af9344a9528
Fernando dos Santos <nando4803@gmail.com>
2020-06-28 17:29:33 +0100

    This commit add navbar, layout and css files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,23 @@
-import React from "react"
-import logo from "../assets/logo.svg"
-import { FaAlignRight } from "react-icons/fa"
-import PageLinks from "../constants/menu"
+import React from "react";
+
+import logo from "../assets/logo.png";
+import { FaAlignRight } from "react-icons/fa";
+import PageLinks from "../constants/menu";
+
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="Logo" />
+          <button type="button" className="toggle-btn">
+            <FaAlignRight />
+          </button>
+        </div>
+        <PageLinks styleClass="nav-links"></PageLinks>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,8 @@
-import React from "react"
-import Links from "../constants/links"
-import SocialLinks from "../constants/socialLinks"
-import { FaTimes } from "react-icons/fa"
+import React from "react";
+import Links from "../constants/menu";
+import SocialLinks from "../constants/socialLinks";
+import { FaTimes } from "react-icons/fa";
+
 const Sidebar = () => {
   return <h2>sidebar component</h2>
 }
