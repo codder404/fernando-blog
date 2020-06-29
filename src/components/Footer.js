617ed479e30bc5bb7fdd12ea6c4a9120fc3dc818
Fernando dos Santos <nando4803@gmail.com>
2020-06-29 12:07:21 +0100

    This commit add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
 import React from "react"
 import SocialLinks from "../constants/socialLinks"
 const Footer = () => {
-  return <h2>footer component</h2>
+  return (
+    <footer className="footer">
+      <div>
+        <SocialLinks styleClass="footer-links"></SocialLinks>
+        <h4>
+          copyright&copy; {new Date().getFullYear()}
+          <span>Fernando dos Santos</span> Todos os direitos reservados
+        </h4>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
