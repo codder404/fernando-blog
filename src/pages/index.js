64bb262888205dab39068aaae022848ab8563a2c
Fernando dos Santos <nando4803@gmail.com>
2020-06-29 10:31:27 +0100

    This commit display blog card to screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,37 @@ import Hero from "../components/Hero";
 import Services from "../components/Services";
 import Blogs from "../components/Blogs";
 
-export default () => {
+export default ({ data }) => {
+  const { allStrapiBlogs: { nodes: blogs } } = data;
+
   return (
     <Layout>
       <Hero />
       <Services />
+      <Blogs blogs={blogs} title="Últimas do blog" showLink />
     </Layout>
   )
 }
-// ...GatsbyImageSharpFluid
+
+export const query = graphql`
+  {
+    allStrapiBlogs(sort: {fields: date, order: DESC}, limit: 3) {
+      nodes {
+        slug
+        content
+        desc
+        date(formatString: "Do MMM, YY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
