24f94e470d634982feafef0f4b8f82bd6f3c418e
Fernando dos Santos <nando4803@gmail.com>
2020-08-07 15:19:41 +0100

    This commit update title component & css

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Title = ({ title }) => {
   return (
-    <div className="section-title">
+    <div className="section-title section-center">
       <h2>{title || "default title"}</h2>
     </div>
   )
