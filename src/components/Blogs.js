ac9f3dc3f79a1e4e00f6a2e590c172a4da8202b1
Fernando dos Santos <nando4803@gmail.com>
2020-08-07 15:33:18 +0100

    This commit update blogs component

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "gatsby";
 
-import Title from "./Title";
 import Blog from "./Blog";
 
 export const Blogs = ({ blogs, title, showLink }) => {
   return (
     <section className="section">
-      <Title title={title} />
       <div className="section-center blogs-center">
         {blogs.map(blog => {
           return <Blog key={blog.id} {...blog} />
