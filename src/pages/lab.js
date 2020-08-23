b5fb41368a2e322093ff3f5b6dd984eee3c6cd4f
Fernando dos Santos <nando4803@gmail.com>
2020-08-23 19:22:01 +0100

    This commit import lab component

--- src/pages/lab.js
+++ src/pages/lab.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+import Lab from '../components/Lab';
 
 function Lab() {
   return (
     <Layout>
       <SEO title="Lab" description="Meu laboratório de projetos" />
-      <section>
-        <h1>Lab</h1>
+      <section className="">
+       <Lab />
       </section>
     </Layout>
   )
