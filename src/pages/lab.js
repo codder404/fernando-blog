cbfc968c70280929fa633355a86780a3a996910e
Fernando dos Santos <nando4803@gmail.com>
2020-08-23 19:14:16 +0100

    This commit add lab

---
+++ src/pages/lab.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+function Lab() {
+  return (
+    <Layout>
+      <SEO title="Lab" description="Meu laboratório de projetos" />
+      <section>
+        <h1>Lab</h1>
+      </section>
+    </Layout>
+  )
+}
+
+export default Lab;
