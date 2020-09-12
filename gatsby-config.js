a852b41c7c4bef144335c2818f277125d80f1c52
Fernando dos Santos <nando4803@gmail.com>
2020-09-12 20:18:59 +0100

    This commit fix gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'Fernando dos Santos',
-    description: `Blog about Front-End`,
+    description: `My Personal Site`,
     author: `@fernando`,
     twitterUsername: `@codder404`,
-    image: '/fcoder.jpg',
     url: 'https://fernandodossantos.netlify.app',
     siteUrl: 'https://fernandodossantos.netlify.app',
   },
   plugins: [
-    {
-      resolve: 'gatsby-plugin-html-attributes',
-      options: {
-        lang: 'pt-ao'
-      }
-    },
-    `gatsby-plugin-emotion`,
-    {
-      resolve: 'gatsby-plugin-styled-components',
-      options: {
-        displayName: false
-      }
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -43,24 +30,15 @@ module.exports = {
     //   },
     // },
     {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `Nunito`,
-          `source sans-serif\:400,700`
-        ],
-        display: 'swap'
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Fernando`,
         lang: `pt-ao`,
-        description: `Desenvolvedor FrontEnd`,
+        description: `My Personal Site`,
         short_name: `Fernando`,
         start_url: `/`,
-        background_color: `#2E3440`,
+        background_color: `#161616`,
+        theme_color: `#161616`,
         dispaly: `standalone`,
         icon: `${__dirname}/static/images/iduck.png`,
       },
