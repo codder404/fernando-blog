f0cc67773fe224d27e1e51dd21e952ed24ee197e
Fernando dos Santos <nando4803@gmail.com>
2020-09-12 18:19:24 +0100

    This commit add all page stuff

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogPage = () => {
+  return (
+    <>
+      <h1>BlogPage</h1>
+    </>
+  )
+}
+
+export default BlogPage;
---
+++ src/pages/lab.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LabPage = () => {
+  return (
+    <>
+      <h1>LabPage</h1>
+    </>
+  )
+}
+
+export default LabPage;
