ebe436c17db27f52f1669b840d4fa534475d1b42
Fernando dos Santos <nando4803@gmail.com>
2020-09-12 21:31:46 +0100

    This commit import hero to indexPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
+import Hero from '../components/Hero';
 
 const IndexPage = () => {
   return (
     <Layout>
-     
+      <Hero />
     </Layout>
   )
 }
