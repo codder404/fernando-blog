149ede99db4fbd3541b64d83a172f26612d6fb0f
Fernando dos Santos <nando4803@gmail.com>
2020-09-12 19:09:08 +0100

    This commit add start layout component

---
+++ src/components/Layout/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StaticQuery, graphql } from 'gatsby';
+
+// import Header from '../Header';
+// import Footer from '../Footer';
+
+import { GlobalStyle, Container, Main } from './styles';
+
+const Layout = ({ children }) => {
+  <StaticQuery query={graphql`
+    query SiteHomeTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `} render={data => {
+    <>
+      <GlobalStyle />
+      {/* <Header siteTilte={data.site.siteMetadata.title} /> */}
+      <Main>
+        <Container>
+          {children}
+        </Container>
+      </Main>
+      {/* <Footer /> */}
+    </>
+  }} 
+
+  />
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default Layout;
