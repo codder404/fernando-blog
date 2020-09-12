8f0e14376c163bd08e40302e9e01f202bf8bf044
Fernando dos Santos <nando4803@gmail.com>
2020-09-12 19:47:16 +0100

    This commit uncomment header

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 
-// import Header from '../Header';
+import Header from '../Header';
 // import Footer from '../Footer';
 
 import { GlobalStyle, Container, Main } from './styles';
@@ -19,7 +19,7 @@ const Layout = ({ children }) => {
   `} render={data => {
     <>
       <GlobalStyle />
-      {/* <Header siteTilte={data.site.siteMetadata.title} /> */}
+      <Header siteTilte={data.site.siteMetadata.title} />
       <Main>
         <Container>
           {children}
