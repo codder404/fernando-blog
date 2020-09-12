4a88ceff13c204c29d43e5ab2cafd07adb79e9a4
Fernando dos Santos <nando4803@gmail.com>
2020-09-12 21:38:07 +0100

    This commit add hero styles

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -8,7 +8,8 @@ const Hero = () => {
     <Container>
       <Section>
         <Title style={{marginBottom: "1rem"}}>Hi There!</Title>
-        <Text>Front-End Developer!</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi suscipit architecto quae nobis reprehenderit laudantium sit quidem similique iusto! Maiores voluptas mollitia beatae praesentium at accusantium similique quibusdam illum reiciendis.</Text>
+        <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi suscipit architecto quae nobis reprehenderit laudantium sit quidem similique iusto! Maiores voluptas mollitia beatae praesentium at accusantium similique quibusdam illum reiciendis.</Text>
       </Section>
     </Container>
   )
--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -1,9 +1,24 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  display: flex;
+  align-items: start;
+  flex-direction: column-reverse;
+  margin-top: 2rem;
+  margin-bottom: 3.5rem;
+
+  @media (min-width: 66.667em) {
+    margin-bottom: 4rem;
+    align-items: center;
+    flex-direction: row;
+  }
 `;
 
 export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
 
+  @media (min-width: 66.667em) {
+    margin-right: 2.5rem;
+  }
 `;
