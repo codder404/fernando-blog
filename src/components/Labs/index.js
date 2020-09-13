c33e5d732936a1bdaf93336b0abd8cb09e5a0a0b
Fernando dos Santos <nando4803@gmail.com>
2020-09-13 15:55:55 +0100

    This commit add labs

---
+++ src/components/Labs/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Labs = () => {
+  return (
+    <Container>
+      <h1>Lab</h1>
+    </Container>
+  )
+}
+
+export default Labs;
