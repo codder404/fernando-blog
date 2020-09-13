d3c8e71d68ebb1dd44d891cec0202680e94b6e96
Fernando dos Santos <nando4803@gmail.com>
2020-09-13 11:22:26 +0100

    This commit update labs

--- src/pages/lab.js
+++ src/pages/lab.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+
 const LabPage = () => {
   return (
-    <>
-      <h1>LabPage</h1>
-    </>
+    <Layout>
+      <SEO title="Lab" />
+      <h1>Em construção!</h1>
+    </Layout>
   )
 }
 
