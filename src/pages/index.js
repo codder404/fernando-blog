470b58df99231e8f2e33c34c87ee7883afa98f2b
Fernando dos Santos <nando4803@gmail.com>
2020-09-13 11:23:34 +0100

    This commit add seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import Layout from '../components/Layout';
 import Hero from '../components/Hero';
 import Blogs from '../components/Blogs';
 import Social from '../components/SocialText';
+import SEO from '../components/SEO';
 
 export default ({ data }) => {
   const { allStrapiBlogs: { nodes: blogs } } = data;
 
   return (
     <Layout>
+      <SEO title="Home" />
       <Hero />
       <Blogs blogs={blogs} />
       <Social />
